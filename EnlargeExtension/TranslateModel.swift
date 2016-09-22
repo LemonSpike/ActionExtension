7d4c6ad356e61320ca6ae534a208c34a1ea66736
Niall Paterson <npaters@tcd.ie>
2016-09-22 15:12:46 +0100

    Update TranslateModel.swift

--- EnlargeExtension/TranslateModel.swift
+++ EnlargeExtension/TranslateModel.swift
@@ -11,14 +11,36 @@ import Alamofire
 
 class TranslateModel: NSObject {
     
+    func bypassAuthentication() {
+
+        let manager = Alamofire.SessionManager.default
+        manager.delegate.sessionDidReceiveChallenge = { session, challenge in
+            var disposition: URLSession.AuthChallengeDisposition = .performDefaultHandling
+            var credential:URLCredential?
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+                disposition = URLSession.AuthChallengeDisposition.useCredential
+                credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+            } else {
+                if challenge.previousFailureCount > 0 {
+                    disposition = .cancelAuthenticationChallenge
+                } else {
+                    credential = manager.session.configuration.urlCredentialStorage?.defaultCredential(for: challenge.protectionSpace)
+                    if credential != nil {
+                        disposition = .useCredential
+                    }
+                }
+            }
+            return (disposition, credential)
+        }
+    }
+    
     func translateText(language: String, text: String) {
-        
+        bypassAuthentication()
         Alamofire.request("https://api-platform.systran.net/translation/text/translate", parameters: ["key":"e1c5251c-4b1c-4808-846f-9fbd8e60a00e","source":"auto","target":language,"input":text]).responseJSON { response in
             print(response)
             DispatchQueue.main.async {
                 let translateResponse = response.result.value
                 print(translateResponse)
-                
             }
         }
         
